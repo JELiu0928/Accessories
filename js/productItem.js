f6dd9bdfeb8b58ab4b119c4f4f4986e9548b8293
je.Liu <pig3703030@gmail.com>
2024-05-31 11:00:59 +0800

    使用js批次加入productItem

---
+++ js/productItem.js
@@ -0,0 +1,125 @@
+
+export let necklaceItems = [
+    {
+        name:'[純銀] 冰凌鎖骨鍊 / 2色 / Ice Necklace',
+        price:792,
+        imgSrc:'./img/necklace/01.webp'
+    },
+    {
+        name:'[鋼飾] 永恆承諾鎖骨鍊 / Eternal Commitment Necklace / 2色',
+        price:363,
+        imgSrc:'./img/necklace/02.webp'
+    },
+    {
+        name:'[鈦鋼] 相同的靈魂項鍊 / Same Soul Necklace',
+        price:495,
+        imgSrc:'./img/necklace/03.webp'
+    },
+    {
+        name:'[純銀] 海的思念鎖骨鍊 / 3色 / Sea Longing Necklace',
+        price:690,
+        imgSrc:'./img/necklace/04.webp'
+    },
+    {
+        name:'[純銀] 挪威之星鎖骨鍊 / Norwegian Star Necklace',
+        price:650,
+        imgSrc:'./img/necklace/05.webp'
+    },
+    {
+        name:'純銀] 心中的星光鎖骨鍊 / 2色 / Starlight In The Heart Necklace',
+        price:1050,
+        imgSrc:'./img/necklace/06.webp'
+    },
+    {
+        name:'挪威之心鎖骨鍊 / 3色 / Heart Of Norway Necklace',
+        price:979,
+        imgSrc:'./img/necklace/07.webp'
+    },
+    {
+        name:'[純銀] 櫻花寓言鎖骨鍊 / Sakura Fable Necklace',
+        price:860,
+        imgSrc:'./img/necklace/08.webp'
+    },
+    {
+        name:'[純銀] 雙子星圈鎖骨鍊 / Gemini Circle Necklace',
+        price:1350,
+        imgSrc:'./img/necklace/09.webp'
+    },
+    {
+        name:'甜美的貝拉鎖骨鍊 / Sweet Bella Necklace',
+        price:560,
+        imgSrc:'./img/necklace/10.webp'
+    },
+    {
+        name:'[鈦鋼] 時間維度鎖骨鍊 / 2色 / Time Dimension Necklace',
+        price:540,
+        imgSrc:'./img/necklace/11.webp'
+    },
+    {
+        name:'[純銀] 奇蹟鹿角鎖骨鍊 / 2色 / Miracle Antlers Necklace',
+        price:825,
+        imgSrc:'./img/necklace/12.webp'
+    },
+]
+export let earringItems = [
+    {
+        name:'盛開的雛菊夾式耳環 Blooming Daisy Ear Clip',
+        price:310,
+        imgSrc:'./img/earrings/01.webp'
+    },
+    {
+        name:'黛妃珍珠耳環2色 Diana Pearl Earring',
+        price:363,
+        imgSrc:'./img/earrings/02.webp'
+    },
+    {
+        name:'[純銀] 圓的流動耳環2色Round Flow Earring',
+        price:450,
+        imgSrc:'./img/earrings/03.webp'
+    },
+    {
+        name:'[採用施華洛世奇水晶純銀] 沉默的高雅耳環12色  Crystals from Swarovski Silent Elegant Earring',
+        price:416,
+        imgSrc:'./img/earrings/04.webp'
+    },
+    {
+        name:'[純銀] 幸運之吻耳環3色 Lucky Kiss Earring',
+        price:257,
+        imgSrc:'./img/earrings/05.webp'
+    },
+    {
+        name:'星辰耳環組2色Star Earring Set',
+        price:462,
+        imgSrc:'./img/earrings/06.webp'
+    },
+    {
+        name:'[純銀] 純真音符耳環3色 Pure Note Earring',
+        price:429,
+        imgSrc:'./img/earrings/07.webp'
+    },
+    {
+        name:'[鋼飾] 沈淪之夏耳環3色Fallen Summer Earring',
+        price:187,
+        imgSrc:'./img/earrings/08.webp'
+    },
+    {
+        name:'[銀針] 黛安娜之星耳環2色Diana Star Earring',
+        price:429,
+        imgSrc:'./img/earrings/09.webp'
+    },
+    {
+        name:'[純銀] 心纏耳環2色Winding Earring',
+        price:560,
+        imgSrc:'./img/earrings/10.webp'
+    },
+    {
+        name:'[銀針] 日光傾城耳環Sunny Allure Earring',
+        price:363,
+        imgSrc:'./img/earrings/11.webp'
+    },
+    {
+        name:'[銀針] 春季水仙花耳環組2色Spring Daffodil Earring Set',
+        price:528,
+        imgSrc:'./img/earrings/12.webp'
+    },
+]
