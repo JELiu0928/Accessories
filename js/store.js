9bddf653ef6c481ab7eb6b535152abf6fc5e1fb4
je.Liu <pig3703030@gmail.com>
2024-05-31 16:45:50 +0800

    新增store.html，用js迭代加入及修正項鍊和耳環畫面渲染

---
+++ js/store.js
@@ -0,0 +1,95 @@
+export const stores = {
+    
+    north:[
+
+        {
+            name:"台北 信義威秀",
+            open:"週日 ~ 週四 | 11:00 - 22:00",
+            open2:"週五 ~ 週六 | 11:00 - 22:30",
+            address:"台北市信義區松壽路20號",
+            map:"https://www.google.com/maps/place/Bonny+%26+Read+%E9%A3%BE%E5%93%81+-+%E5%8F%B0%E5%8C%97%E4%BF%A1%E7%BE%A9%E5%A8%81%E7%A7%80%E9%96%80%E5%B8%82/@25.0356356,121.5649102,17z/data=!3m1!4b1!4m5!3m4!1s0x3442abb09c441235:0xe9a5db22cc3bfe34!8m2!3d25.0356356!4d121.5670989"
+        },
+        {
+            name:"台北 西門萬年",
+            open:"週日 ~ 週四 | 11:00 - 22:00",
+            open2:"週六 ~ 週日 | 12:00 - 22:30",
+            address:"台北市萬華區西寧南路70-19號",
+            map:"https://www.google.com/maps/place/Bonny%26Read%E9%A3%BE%E5%93%81+%E5%8F%B0%E5%8C%97%E8%A5%BF%E9%96%80%E7%94%BA%E8%90%AC%E5%B9%B4%E9%96%80%E5%B8%82/@25.0435081,121.505886,15z/data=!4m5!3m4!1s0x0:0x1c589559ec341ab1!8m2!3d25.0435081!4d121.505886"
+        },
+        {
+            name:"新北中和 環球購物中心",
+            open:"  OPEN / 週一 ~ 週日 | 11:00 - 22:00",
+            address:"新北市中和區中山路三段122號(2F)",
+            map:"https://www.google.com.tw/maps/place/Bonny+%26+Read%E9%A3%BE%E5%93%81+%E6%96%B0%E5%8C%97%E4%B8%AD%E5%92%8C%E7%92%B0%E7%90%83%E9%96%80%E5%B8%82/@25.0061984,121.4751139,15z/data=!4m2!3m1!1s0x0:0x634d9e7f1768778c?sa=X&hl=zh-TW&ved=2ahUKEwjIpcy15Jz3AhXHw4sBHbCsAekQ_BJ6BAhQEAU"
+        },
+        {
+            name:"新北 新店裕隆城",
+            open:"週日 ~ 週四 | 11:00 - 21:30",
+            open2:"週五 ~ 週六 | 11:00 - 22:00",
+            address:"新北市新店區中興路三段三段70號(4F)",
+            map:"https://www.google.com/maps/place/Bonny+%26+Read%E9%A3%BE%E5%93%81+%E6%96%B0%E5%8C%97%E6%96%B0%E5%BA%97%E8%A3%95%E9%9A%86%E5%9F%8E%E6%AB%83%E4%BD%8D/@24.978061,121.5439031,17z/data=!3m1!4b1!4m6!3m5!1s0x346801141f0a6c01:0xe1d46ff617f8f648!8m2!3d24.978061!4d121.546478!16s%2Fg%2F11vdldx56s?entry=ttu"
+
+        },
+    ],
+    central:[
+
+        {
+            name:"台中 大遠百",
+            open:"週一 ~ 週五 | 11:00 - 22:00",
+            open2:"週六 ~ 週日 | 10:30 - 22:00",
+            address:"台中市西屯區台灣大道三段251號 ",
+            map:"https://www.google.com.tw/maps/place/Bonny+%26+Read%E9%A3%BE%E5%93%81+%E5%8F%B0%E4%B8%AD%E5%A4%A7%E9%81%A0%E7%99%BE%E9%96%80%E5%B8%82/@24.1637002,120.6446167,15z/data=!4m2!3m1!1s0x0:0xac8ee0d2fcb3e2b9?sa=X&hl=zh-TW&ved=2ahUKEwjb9dzp5Zz3AhXkyosBHQRtCeAQ_BJ6BAhcEAU"
+
+        },
+        {
+            name:"台中 大魯閣新時代",
+            open:"週一 ~ 週五 | 11:00 - 22:00",
+            open2:"週六 ~ 週日 | 10:30 - 22:00",
+            address:"台中市東區復興路四段186號(6F)",
+            map:"https://www.google.com.tw/maps/place/Bonny+%26+Read%E9%A3%BE%E5%93%81+%E5%8F%B0%E4%B8%AD%E5%A4%A7%E9%AD%AF%E9%96%A3%E6%96%B0%E6%99%82%E4%BB%A3%E6%AB%83%E4%BD%8D/@24.1357726,120.6877477,15z/data=!4m6!3m5!1s0x34693d02abb466a1:0x9022e7fe2ab5ea5!8m2!3d24.1357726!4d120.6877477!16s%2Fg%2F11qrsfvzlb?hl=zh-TW&entry=ttu"
+
+        },
+        {
+            name:"台中 誠品生活480",
+            open:" 週日 ~ 週四 | 11:00 - 22:00",
+            address:"台中市西屯區市政路480號(3F)",
+            map:"https://www.google.com/maps/place/403%E5%8F%B0%E4%B8%AD%E5%B8%82%E8%A5%BF%E5%8D%80%E7%BE%8E%E6%9D%91%E8%B7%AF%E4%B8%80%E6%AE%B5117%E5%B7%B75%E8%99%9F/@24.1522416,120.6518933,15z/data=!3m1!4b1!4m14!1m7!3m6!1s0x34693d934d275703:0xdfd69f4c46a82ccf!2zQm9ubnkgJiBSZWFk6aO-5ZOBIOWPsOS4reWLpOe-jumWgOW4gg!8m2!3d24.1522419!4d120.662193!16s%2Fg%2F11lclz_x3p!3m5!1s0x34693d9c3bcfbbaf:0x8a0f71b667816028!8m2!3d24.1522419!4d120.662193!16s%2Fg%2F11c5p0gs3_?entry=ttu"
+
+        }
+    ],
+    south:[
+        {
+            name:"嘉義 秀泰百貨",
+            open:"週一 ~ 週日 | 11:00 - 22:00",
+            address:"嘉義市西區文化路299號(2F)",
+            map:"https://www.google.com.tw/maps/place/Bonny+%26+Read%E9%A3%BE%E5%93%81+%E5%98%89%E7%BE%A9%E7%A7%80%E6%B3%B0%E6%AB%83%E4%BD%8D/@23.4852891,120.4476782,15z/data=!4m2!3m1!1s0x0:0xddb50a29a64c20aa?sa=X&hl=zh-TW&ved=2ahUKEwjWncCZ5pz3AhUKyosBHXQdC6oQ_BJ6BAhDEAU"
+
+        },
+        {
+            name:"台南 新光三越百貨小西門",
+            open:"週一 ~ 週日 | 11:00 - 22:00 ",
+            address:"台北市信義區松壽路20號",
+            map:"https://www.google.com.tw/maps/place/Bonny+%26+Read%E9%A3%BE%E5%93%81+%E5%8F%B0%E5%8D%97%E6%96%B0%E5%85%89%E4%B8%89%E8%B6%8A%E5%B0%8F%E8%A5%BF%E9%96%80%E9%96%80%E5%B8%82/@22.9873418,120.1991126,15z/data=!4m2!3m1!1s0x0:0x380368b87bb78f4a?sa=X&hl=zh-TW&ved=2ahUKEwjzxc-x5pz3AhW9yosBHRq-AWwQ_BJ6BAg-EAU"
+
+        },
+        {
+            name:"高雄 新堀江",
+            open:"週一 ~ 週日 | 14:00 - 22:00 ",
+            address:"高雄市新興區新田路130號",
+            map:"https://www.google.com.tw/maps/place/Bonny+%26+Read+%E9%A3%BE%E5%93%81+-+%E9%AB%98%E9%9B%84%E6%96%B0%E5%B4%9B%E6%B1%9F%E9%96%80%E5%B8%82/@22.6222656,120.3011615,17z/data=!4m13!1m7!3m6!1s0x346e0484202ca269:0x46ac37fbf5398e80!2zODAw6auY6ZuE5biC5paw6IiI5Y2A5paw55Sw6LevMTMw6Jmf!3b1!8m2!3d22.6222656!4d120.3033502!3m4!1s0x346e0484202bf4b9:0x6aa4d57354189e99!8m2!3d22.6222656!4d120.3033502?utm_campaign=0315%E6%96%B0%E5%B4%9B%E6%B1%9F%E9%96%80%E5%B8%82+-+%E5%AE%98%E7%B6%B2%E5%9C%B0%E5%9C%96&utm_source=br&utm_medium=br+map&utm_term=0315%E6%96%B0%E5%B4%9B%E6%B1%9F%E9%96%80%E5%B8%82+-+%E5%AE%98%E7%B6%B2%E5%9C%B0%E5%9C%96"
+
+        },
+        {
+            name:"高雄 左營新光三越彩虹市集 ",
+            open:"週一 ~ 週日 | 11:00 - 22:00 ",
+            address:"高雄市左營區高鐵115號(3F) ",
+            map:"https://www.google.com.tw/maps/place/Bonny+%26+Read%E9%A3%BE%E5%93%81+%E9%AB%98%E9%9B%84%E5%B7%A6%E7%87%9F%E6%96%B0%E5%85%89%E4%B8%89%E8%B6%8A%E9%96%80%E5%B8%82/@22.6878238,120.3092282,15z/data=!4m2!3m1!1s0x0:0x33d45a979093aec5?sa=X&hl=zh-TW&ved=2ahUKEwjs5fvA5pz3AhUHrpQKHcpFB2QQ_BJ6BAhQEAU"
+
+        }
+    ],
+    
+
+            
+    
+      
+}
